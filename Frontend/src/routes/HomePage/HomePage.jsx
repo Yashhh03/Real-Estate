b7e4a6cee23d8b7dcf1f49e7227feba491d84630
Yashhh03 <yashsurya8888@gmail.com>
2024-06-14 15:27:34 +0530

    HomePage react

--- Frontend/src/routes/HomePage/HomePage.jsx
+++ Frontend/src/routes/HomePage/HomePage.jsx
@@ -1,9 +1,38 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
 import "./HomePage.scss";
 
 export default function HomePage() {
   return (
     <div className="homePage">
-      <></>
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Your Perfect Place </h1>
+
+          <p>
+            Experience luxurious living in our contemporary urban oasis, where
+            modern design meets unparalleled comfort. Welcome to a prestigious
+            address redefining upscale living.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Awards Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" />
+      </div>
     </div>
   );
 }
